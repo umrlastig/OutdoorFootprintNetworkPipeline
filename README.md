b0c4f45915e2bf4ad5a39f5d885fb47327f8a46c
Marie-Dominique Van Damme <marie-dominique.vandamme@ign.fr>
2026-02-08 12:32:54 +0100

    decoup et resample

--- README.md
+++ README.md
@@ -17,14 +17,89 @@ Source code for creating an outdoor activity footprint network from GNSS traject
 
 
 
+## Script *1_decoup_et_resample.py*: Découpage et ré-échantillonnage des traces en entrée
+          
+=> produit un jeu de traces, résolues spatialement à 1 mètre, 
+                    extraites (peut-être découpées) suivant une figure géométrique
 
-## Prerequisites
+Le script peut être lancer sans QGIS, dans ce cas il faut commenter 
 
-You need to install the following libraries:
+
+Paramètres à renseigner :
+--------------------------
+
+* RESAMPLE_SIZE = 1
+
+* NB_OBS_MIN    = 10 # nombre de points minimum pour une trace
+
+* DIST_MAX_2OBS = 50 # si supérieur on coupe la trace
+
+* tracescsvpath = r'/home/md_vandamme/5_GPS/OV/BAUGES/run/'
+
+* Limites de la zone d'étude sous forme de coordonnées des sommets des vertex :
+
+X = [945878, 956330, 955879, 954402, 952511, 950389, 948774, 945857, 945878]
+Y = [6516870, 6516805, 6508417, 6506849, 6506503, 6505649, 6504150, 6503762, 6516870]
+
+* Le répertoire qui va contenir les nouvelles traces : découpées et ré-échantillonnées 
+
+Il faut créer le répertoire avant de lancer le script.
+
+tracespath = r'/home/md_vandamme/4_RESEAU/ExampleRunning/traces/'
+
+
+
+
+
+En sortie :
+
+
+## 
+
+
+
+
+# Environment Setup
+
+## Requirements
+
+OutdoorFootprintNetworkPipeline requires the following Python packages and Plugin QGIS:
 
 - Tracklib
 - GeoNetLib
 - Plugin QGis "SciPy Filters"
+
+
+The environment setup depends on your profile: development (if you need to modify Tracklib or Geonetlib), or user (if you just want to run the pipeline on a use case).
+
+
+### Development Environment Setup
+
+
+1. You need to install the following libraries:
+
+- clone the Tracklib from github
+- clone the GeoNetLib from github
+- Plugin QGis "SciPy Filters"
+
+
+2. Comme les scripts vont être exécutés dans QGis, il faut configuer à QGis ces deux librairies:
+
+Cliquer dans la barre de menu sur Préférences >> Options >> Système >> 
+
+Puis dans le bloc "Environnement", ajouter une variable personnalisée:
+
+*Appliquer* : ajouter au début
+*Variable*  : PYTHONPATH
+*Valeur*    : /home/md_vandamme/7_LIB/tracklib:/home/md_vandamme/7_LIB/GeoNetLib
+
+
+
+### "Just want to run the pipeline on a use case" Environment Setup
+
+1.
+
+
 
 
 ## How to Run the Code
--- source/1_decoup_et_resample.py
+++ source/1_decoup_et_resample.py
@@ -1,22 +1,39 @@
 # -*- coding: utf-8 -*-
 
-'''
+
 import matplotlib.pyplot as plt
 import tracklib as tkl
 from tracklib.util.qgis import QGIS, LineStyle, PointStyle
 
-RESAMPLE_SIZE = 1
-G1_SIZE = 2
-G2_SIZE = 50
 
+# =============================================================================
+#       INPUT - OUTPUT
+#
+
+# données en entrée
+RESAMPLE_SIZE = 1
+NB_OBS_MIN    = 10 # nombre de points minimum pour une trace
+DIST_MAX_2OBS = 50 # si supérieur on coupe la trace
+tracescsvpath = r'/home/md_vandamme/5_GPS/OV/BAUGES/run/'
 
 X = [945878, 956330, 955879, 954402, 952511, 950389, 948774, 945857, 945878]
 Y = [6516870, 6516805, 6508417, 6506849, 6506503, 6505649, 6504150, 6503762, 6516870]
 
-poly = tkl.Polygon(X, Y)
-constraintBBox = tkl.Constraint(shape=poly, mode=tkl.MODE_CROSSES,
-                            type=tkl.TYPE_CUT_AND_SELECT)
+# X = [949798, 950234, 951228, 951259, 950326, 950120, 950298, 949766, 949329, 949138, 949145, 949340, 949397, 949457, 949798]
+# Y = [6513065, 6513079, 6512862, 6512504, 6512529, 6512224, 6511908, 6511248, 6510989, 6511152, 6511415, 6511794, 6512337, 6513104, 6513065]
 
+# données en sortie
+# Le répertoire doit être créé
+tracespath = r'/home/md_vandamme/4_RESEAU/ExampleRunning/traces/'
+
+
+# =============================================================================
+#         LECTURE de toutes les traces
+#
+poly = tkl.Polygon(X, Y)
+constraintBBox = tkl.Constraint(shape=poly,
+                                mode=tkl.MODE_CROSSES,
+                                type=tkl.TYPE_CUT_AND_SELECT)
 
 fmt = tkl.TrackFormat({'ext': 'CSV',
                        'srid': 'ENU',
@@ -27,12 +44,23 @@ fmt = tkl.TrackFormat({'ext': 'CSV',
                        'cmt': '#',
                        'read_all': True})
 
-# z1path = r'/home/md_vandamme/3_IntForOut/DATA/OV/BAUGES/zone1/'
-z2path = r'/home/md_vandamme/5_GPS/OV/BAUGES/walk/'
-collection2 = tkl.TrackReader.readFromFile(z2path, fmt)
+collection2 = tkl.TrackReader.readFromFile(tracescsvpath, fmt)
+print ('Fin de la lecture des données 1/2.')
 
+
+# =============================================================================
+#         Découpage et ré-échantillonnage
+#
+
+print ('Début découpage et ré-échantillonnage')
+
+# On prépare la nouvelle collection de traces
+cpt = 1
 collection = tkl.TrackCollection()
 for trace in collection2:
+    
+    trace.resample(RESAMPLE_SIZE, tkl.MODE_SPATIAL)
+
     selection = constraintBBox.select(tkl.TrackCollection([trace]))
     if len(selection) <= 0:
         continue
@@ -41,32 +69,51 @@ for trace in collection2:
     tn = tkl.Track()
     for o2 in selection.getTrack(0):
         if o1 is not None:
-            if o1.distance2DTo(o2) > 50:
+            if o1.distance2DTo(o2) > DIST_MAX_2OBS:
                 # on coupe la trace
-                if tn.size() >= 10:
+                if tn.size() >= NB_OBS_MIN:
+                    tn.uid = trace.tid
+                    tn.tid = trace.tid
                     collection.addTrack(tn)
                 tn = tkl.Track()
         tn.addObs(o2)
         o1 = o2
+
     # dernière trace
-    if tn.size() >= 10:
+    if tn.size() >= NB_OBS_MIN:
         collection.addTrack(tn)
 
+    if cpt%100 == 0:
+        print ('    ', cpt, '/', collection2.size())
+    cpt += 1
 
-for trace in collection:
-    trace.uid = trace.tid
-    trace.resample(RESAMPLE_SIZE, tkl.MODE_SPATIAL)
+
 print ('Number of tracks in zone1: ' + str(collection.size()))
 
-QGIS.plotTracks(collectionZone1, type='LINE',
-                style=LineStyle.simpleBlue, title='Zone2-walk')
-'''
+
+tkl.TrackWriter.writeToFiles(collection, tracespath,
+                             id_E=1, id_N=0, id_U=3, id_T=2,
+                             h=1, separator=";")
+
+print ('Fin découpage et ré-échantillonnage 2/3.')
 
 
 
+# =============================================================================
+#         Affichage des résultats dans QGis
+#
+
+QGIS.plotTracks(collection, type='LINE',
+                style=LineStyle.simpleBlue,
+                title='Traces ré-échantillonnées et découpées')
+QGIS.plotTracks(collection, type='POINT',
+                title='Traces ré-échantillonnées et découpées')
 
 
+print ("Fin de l'affichage des données 3/3.")
 
+
+print ("END script 1.")
 
 
 
