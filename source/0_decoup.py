552740a862cc289acd6d34056b351142bca75f56
Marie-Dominique Van Damme <marie-dominique.vandamme@ign.fr>
2026-02-08 18:09:51 +0100

    decoupage

---
+++ source/0_decoup.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+
+
+import tracklib as tkl
+from tracklib.util.qgis import QGIS, LineStyle, PointStyle
+
+
+""" ======================================================================= """
+"""     PARAMETRES  INPUT-OUTPUT                                            """
+"""                                                                         """
+
+# -----------------------------------------------------------------------------
+# Paramètre 1 : Nombre de points minimum pour un morceau de trace au moment du découpage
+#               si le nombre n'est pas atteint, le morceau de trace est oublié
+
+NB_OBS_MIN    = 10
+
+
+# -----------------------------------------------------------------------------
+# Paramètre 2 : Distance en mètres entre 2 points,
+#               si supérieure au seuil on coupe la trace
+
+DIST_MAX_2OBS = 50
+
+
+# -----------------------------------------------------------------------------
+# Paramètre 3 : chemin/répertoire où sont stockés les traces Outdoorvision:
+#                           un pré-traitement doit déjà avoir été fait (Cf FTP):
+#                           un fichier CSV par trace
+
+# tracespathsource = r'/home/md_vandamme/5_GPS/OV/BAUGES/run/'
+tracespathsource = r'/home/md_vandamme/5_GPS/OV/BAUGES/walk/'
+
+
+# -----------------------------------------------------------------------------
+# Paramètre 4: Coordonnées de la zone d'étude sur laquelle on construit le réseau
+#                           Polygone sous la forme d'un tableau de X et de Y
+
+# traces de la zone 1 (10km x 10km)
+#X = [945878, 956330, 955879, 954402, 952511, 950389, 948774, 945857, 945878]
+#Y = [6516870, 6516805, 6508417, 6506849, 6506503, 6505649, 6504150, 6503762, 6516870]
+
+# traces de la zone 2 (1km x 1km)
+X = [949798, 950234, 951228, 951259, 950326, 950120, 950298, 949766, 949329, 949138, 949145, 949340, 949397, 949457, 949798]
+Y = [6513065, 6513079, 6512862, 6512504, 6512529, 6512224, 6511908, 6511248, 6510989, 6511152, 6511415, 6511794, 6512337, 6513104, 6513065]
+
+
+# -----------------------------------------------------------------------------
+# Paramètre 5 : répertoire créé, où les données en sortie vont être enregistrées
+#               ce sera l'entrée du prochain script
+#
+#tracespath = r'/home/md_vandamme/4_RESEAU/ExampleRunning/raw_walk_ze1/'
+tracespath = r'/home/md_vandamme/4_RESEAU/ExampleTest/raw_walk_ze2/'
+
+
+
+""" ======================================================================= """
+"""         LECTURE de toutes les traces                                    """
+"""                                                                         """
+
+fmt = tkl.TrackFormat({'ext': 'CSV',
+                       'srid': 'ENU',
+                       'id_E': 1,'id_N': 0, 'id_U': 3,'id_T': 2,
+                       'time_fmt': '2D/2M/4Y 2h:2m:2s',
+                       'separator': ';',
+                       'header': 0,
+                       'cmt': '#',
+                       'read_all': True})
+
+collection2 = tkl.TrackReader.readFromFile(tracespathsource, fmt)
+print ('Fin de la lecture des données 1/3.')
+
+
+
+
+""" ======================================================================= """
+"""         Découpage                                                       """
+"""                                                                         """
+
+print ('    Début découpage.')
+
+poly = tkl.Polygon(X, Y)
+constraintBBox = tkl.Constraint(shape=poly,
+                                mode=tkl.MODE_CROSSES,
+                                type=tkl.TYPE_CUT_AND_SELECT)
+
+# On prépare la nouvelle collection de traces
+cpt = 1
+collection = tkl.TrackCollection()
+for trace in collection2:
+
+    if cpt%200 == 0:
+        print ('   ', cpt, '/', collection2.size())
+    cpt += 1
+    
+    selection = constraintBBox.select(tkl.TrackCollection([trace]))
+    if len(selection) <= 0:
+        continue
+
+    o1 = None
+    tn = tkl.Track()
+    for o2 in selection.getTrack(0):
+        if o1 is not None:
+            if o1.distance2DTo(o2) > DIST_MAX_2OBS:
+                # on coupe la trace
+                if tn.size() >= NB_OBS_MIN:
+                    tn.uid = trace.tid
+                    tn.tid = trace.tid
+                    collection.addTrack(tn)
+                tn = tkl.Track()
+        tn.addObs(o2)
+        o1 = o2
+
+    # dernière trace
+    if tn.size() >= NB_OBS_MIN:
+        collection.addTrack(tn)
+
+
+print ('    Nombre de traces après découpage : ' + str(collection.size()))
+print ('Fin découpage 2/3.')
+
+
+
+
+
+""" ======================================================================= """
+"""         Affichage des résultats dans QGis                               """
+"""                                                                         """
+
+QGIS.plotTracks(collection, type='LINE',
+                style=LineStyle.simpleBlue,
+                title='Raw lines')
+QGIS.plotTracks(collection, type='POINT',
+                style=PointStyle.simpleSquareBlue,
+                title='Raw points')
+
+tkl.TrackWriter.writeToFiles(collection, tracespath,
+                             id_E=1, id_N=0, id_U=3, id_T=2,
+                             h=1, separator=";")
+
+print ("Fin de l'enregistrement et affichage des données dans QGIS 3/3.")
+
+
+
+""" ======================================================================= """
+"""           FIN                                                           """
+"""                                                                         """
+print ("END SCRIPT 0.")
+
+
+
+
