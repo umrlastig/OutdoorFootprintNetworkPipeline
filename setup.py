ed7736e1c738c6ae2ae735233b0cff107600dbf9
Marie-Dominique Van Damme <marie-dominique.vandamme@ign.fr>
2026-06-03 21:53:48 +0200

    v1.0.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup (
     description="footprint2graph is an open-source Python processing pipeline (MIT license) for generating outdoor activity footprint networks from GNSS trajectories, representing, for example, hikers’ or runners’ footprints within a defined spatial and temporal extent.",
     long_description="See ...",
     url="https://github.com/umrlastig/footprint2graph",
-    download_url= 'https://github.com/umrlastig/Footprint2graph-pipeline/archive/refs/tags/0.1b1.zip',
+    download_url= 'https://github.com/umrlastig/footprint2graph/archive/refs/tags/v1.0.0.zip',
     author="Marie-Dominique Van Damme, Yann Méneroux",
     author_email="todo@ign.fr",
     keywords=[],
